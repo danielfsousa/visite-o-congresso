c97802fe5dafeee990bd1175ff6491bac6320b5e
Danilo Lemes <daniloleemes@hotmail.com>
2018-09-15 19:38:26 -0300

    ajustes como chegar

---
+++ screens/ComoChegarScreen.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import { StyleSheet, ScrollView, View } from 'react-native'
+
+import { Colors, Images, Layout } from '../constants'
+import { Title } from '../components/StyledText'
+import Hyperlink from '../components/Hyperlink'
+import BackgroundImage from '../components/BackgroundImage'
+import Header from '../components/Header'
+import AccordionView from '../components/AccordionView'
+
+class ComoChegarScreen extends Component {
+  infos = [
+    {
+      title: 'De ônibus',
+      text: 'As linhas 152, 152.2, 152.3, 162, 161, 108, 108.3 e 108.5  passam pela Esplanada dos Ministérios e pela Praça dos Três Poderes'
+    },
+    {
+      title: 'De micro-ônibus (zebrinha)',
+      text: 'As linhas 07, 08, 11, 16, 24, 25, 31, 32 e 113 passam pela Esplanada dos Ministérios e pela Praça dos Três Poderes'
+    },
+    {
+      title: 'De táxi',
+      text: 'Peça ao taxista para deixá-lo na Chapelaria do Congresso Nacional. Nesse local também há um ponto de táxi caso precise de um após a visita. Na Chapelaria informe que deseja ir ao Salão Negro.\nAtenção: o ponto de táxi não funciona nos finais de semana e nos feriados.'
+    },
+    {
+      title: 'De metrô',
+      text: 'Desça na estação da rodoviária do Plano Piloto (ponto final) e pegue um dos ônibus que passam pela Praça dos Três Poderes: 108, 108.3 e 108.5'
+    },
+    {
+      title: 'De carro',
+      text: 'Como são vários os caminhos possíveis para chegar à Esplanada dos Ministérios, o jeito mais fácil é avaliar as possibilidades por mapas da internet. Quem vier de carro poderá estacionar em vagas ao longo das vias de acesso ao Congresso'
+    },
+    {
+      title: 'A partir do aeroporto',
+      text: 'A linha executiva de ônibus 113, parte  do aeroporto de Brasília  e passa pela Praça dos Três Poderes.'
+    }
+  ]
+
+  static navigationOptions = ({ navigation }) => ({
+    header: (
+      <Header
+        transparent
+        detach
+        type='big'
+        onBackButtonClick={() => navigation.goBack()}>
+        Como chegar
+      </Header>
+    )
+  })
+
+  render () {
+    return (
+      <BackgroundImage
+        source={Images.backgroundFade}
+        style={styles.container}
+        imageStyle={styles.backgroundImage}
+        overlayStyle={styles.overlay}>
+
+        <ScrollView
+          alwaysBounceVertical={false}
+          overScrollMode='never'
+          style={styles.scrollContainer}>
+
+          <View style={styles.titleContainer}>
+            <Title style={styles.title}>Para mais informações como itinerários dos ônibus e mapa, consultar:</Title>
+            <Hyperlink style={styles.link} href='http://www.horarios.dftrans.df.gov.br'>Site DFTrans</Hyperlink>
+            <Hyperlink style={styles.link} href='https://maps.google.com/maps?f=q&source=s_q&hl=pt-BR&geocode=&q=congresso+nacional&aq=&sll=37.0625,-95.677068&sspn=51.089971,93.076172&vpsrc=0&t=h&ie=UTF8&hq=&hnear=&z=17&iwloc=A&cid=411739593941032767'>Mapa</Hyperlink>ß
+          </View>
+
+          {
+            this.infos.map((props, i) => <AccordionView key={props.title} {...props} index={i} />)
+          }
+
+        </ScrollView>
+
+      </BackgroundImage>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    marginBottom: 13
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+
+  overlay: {
+    backgroundColor: Colors.rgba(Colors.background, 96)
+  },
+
+  title: {
+    marginBottom: 15,
+    paddingHorizontal: Layout.padding,
+    textAlign: 'justify'
+  },
+
+  link: {
+    marginBottom: 10,
+    paddingHorizontal: Layout.padding
+  },
+
+  scrollContainer: {
+    marginTop: 130
+  }
+
+})
+
+export default ComoChegarScreen
